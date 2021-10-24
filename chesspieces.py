fd4ecdc0b465afeed5c5d9cf256f170575f6d2d0
mkulkar <mkulkar@gmu.edu>
2021-10-23 21:31:42 -0400

    board numbers/letters

--- chesspieces.py
+++ chesspieces.py
@@ -11,15 +11,16 @@ board = [['R', 'K', 'B', 'C', 'Q', 'B', 'K', 'R'],
 
 
 def print_board(board: list):
-    final_str = '=' * 30 + '\n'
+    final_str = '    ' + '=' * 30 + '\n'
     for r, row in enumerate(board):
-        row_str = row[0]
+        row_str = '{} ~ '.format(r +1)
+        row_str += row[0]
         for c, cell in enumerate(row):
             if c == 0:
                 continue
             row_str += ' | ' + row[c]
-        final_str += row_str + '\n' + "-" * 30 + '\n'
-    final_str += '=' * 30
+        final_str += row_str + '\n' + '    ' + "-" * 30 + '\n'
+    final_str += '    ' + '=' * 30 + '\n' + '    H   G   F   E   D   C   B   A'
     print(final_str)
 
 
